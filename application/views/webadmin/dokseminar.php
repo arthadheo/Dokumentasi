2cbada261a2fefac74d408ad20ab1d7864162e09
Dheo Artha <dheo@hukumonline.com>
2022-08-02 15:25:19 +0700

    hapus file mangsi, menambahkan file dokumen

--- application/views/webadmin/dokseminar.php
+++ application/views/webadmin/dokseminar.php
@@ -1,18 +1,20 @@
-<div class="d-sm-flex justify-content-between">
-    <h1 class="h5 mb-0 text-gray-800">Dheo Artha</h1>
-</div>
-<div class="d-sm-flex justify-content-between">
-    <h1 class="h5 mb-0 text-gray-800">105216035</h1>
-</div>
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h5 mb-0 text-gray-800">Ilmu Komputer</h1>
-</div>
+<div style="padding: 24px;">
+    <div class="d-sm-flex justify-content-between" style="padding: 12px;">
+        <h1 class="h5 mb-0 text-gray-800">Dheo Artha</h1>
+    </div>
+    <div class="d-sm-flex justify-content-between">
+        <h1 class="h5 mb-0 text-gray-800">105216035</h1>
+    </div>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h5 mb-0 text-gray-800">Ilmu Komputer</h1>
+    </div>
 
-<div class="row">
-    <div class="col-xl-6 col-md-4 mb-4">
-        <div class="mb-3">
-        <label for="formFile" class="form-label">Dokumen Proposal Tugas Akhir</label>
-        <input class="form-control" type="file" id="formFile1">
+    <div class="row" style="padding:36px;">
+        <div class="col-xl-6 col-md-4 mb-4">
+            <div class="mb-3">
+            <label for="formFile" class="form-label">Dokumen Proposal Tugas Akhir</label>
+            <input class="form-control" type="file" id="formFile1">
+            </div>
         </div>
     </div>
 </div>
